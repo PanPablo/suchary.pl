63d81afcd012e09f1943d8ca42faae14203cfe52
Pawel Struminski <pstruminski@gmail.com>
2017-11-24 09:44:17 +0100

    male poprawki responsywnosci

--- wp-content/themes/suchary/header.php
+++ wp-content/themes/suchary/header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <?php wp_head(); ?>
