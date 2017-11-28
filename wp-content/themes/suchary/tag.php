596359822bf1a1b9c34d1606df43cdcc95947e1d
Pawel Struminski <pstruminski@gmail.com>
2017-11-28 09:12:36 +0100

    rating dla tagow

--- wp-content/themes/suchary/tag.php
+++ wp-content/themes/suchary/tag.php
@@ -8,33 +8,45 @@
 ?>
 <?php get_header(); ?>
     <div class="container">
-<?php if ( have_posts() ) : while ( have_posts() ) :    the_post(); ?>
-    <div class="row">
-        <div class="col-6-6">
-            <div class="singleJoke">
-                <h4>Autor:<?php the_author() ?></h4>
-                <p class="tag"><?php the_tags()?></p>
-                <div class="image"><?php the_post_thumbnail('thumbnail') ?></div>
-                <span><?php the_content() ?></span>
-                <div><?php if(function_exists('the_ratings')) { the_ratings(); } ?></div>
-                <div
-                    class="fb-like"
-                    data-share="true"
-                    data-width="450"
-                    data-show-faces="true"
-                    data-layout="button"
-                    data-action="like"
-                    data-size="large">
+        <?php
+
+        $q = new WP_Query([
+        'posts_per_page' => 10,
+        'meta_key' => 'ratings_average',
+        'orderby' => 'meta_value_num',
+        'order' => 'DESC',
+
+        ]);
+
+        if ( $q->have_posts() ) : while ( $q->have_posts() ) : $q->the_post(); ?>
+        <!-- post -->
+        <div class="row">
+            <div class="col-6-6">
+                <div class="singleJoke">
+                    <h4>Autor:<?php the_author() ?></h4>
+                    <p class="tag"><?php the_tags()?></p>
+                    <div class="image"><?php the_post_thumbnail('thumbnail') ?></div>
+                    <span><?php the_content() ?></span>
+                    <div><?php if(function_exists('the_ratings')) { the_ratings(); } ?></div>
+                    <div
+                            class="fb-like"
+                            data-share="true"
+                            data-width="450"
+                            data-show-faces="true"
+                            data-layout="button"
+                            data-action="like"
+                            data-size="large">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-<?php endwhile; ?>
-  <!-- post navigation -->
-<?php else: ?>
-  <!-- no posts found -->
-<?php endif; ?>
-
+        <br>
+        <?php endwhile; ?>
+        <!-- post navigation -->
+        <?php else: ?>
+            <!-- no posts found -->
+        <?php endif;
+?>
     <div class="row">
         <div class="col-6-6 pag">
             <div class="pagination"><?php post_pagination(); ?></div>
